4f3e8da2a26e37bf4c29cb8af3a2a456c7783c0d
bincrafters-user <bincrafters@gmail.com>
2021-12-18 03:37:33 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.31.0

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,6 +6,6 @@ class TestPackageConan(ConanFile):
     settings = "os", "arch"
 
     def test(self):
-        if not tools.cross_building(self.settings):
+        if not tools.cross_building(self):
             file_path = os.path.join(self.source_folder, "conan_small.png")
             self.run("basisu {}".format(file_path), run_environment=True)
